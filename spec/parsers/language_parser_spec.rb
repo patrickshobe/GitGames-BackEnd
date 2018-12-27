99e6f3c7db458e7e6d2c9bf18b1f83fc930a4621
Tara Craig <tara.craig7@gmail.com>
2018-12-27 16:38:16 -0700

    Add test for parsing language usage totals

---
+++ spec/parsers/language_parser_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Language Query Spec' do
+  it 'can parse language query info' do
+    VCR.use_cassette('language_parser_happy') do
+      username = 'tcraig7'
+
+      parsed_language = LanguageParser.new
+      parsed_language.get_data(username)
+
+      result = parsed_language.languages_breakdown
+
+      expect(result).to be_a(Hash)
+      expect(result).to have_key("Ruby")
+      expect(result).to have_key("CSS")
+    end
+  end
+end
