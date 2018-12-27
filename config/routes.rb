2dcbb153c7d7b3585a1cc5a3b53bd0f8f69127df
Patrick Shobe <patrick@patrickshobe.com>
2018-12-26 18:42:10 -0700

    Add route and controller for commit message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index]
+      resources :commit_messages, only: [:index]
     end
   end
 end
