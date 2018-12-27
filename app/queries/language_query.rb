44e512a745753c2d09ecb4d81bf3bb52e3a5b3d8
Tara Craig <tara.craig7@gmail.com>
2018-12-27 13:41:32 -0700

    Add methods to make language query

---
+++ app/queries/language_query.rb
@@ -0,0 +1,44 @@
+class LanguageQuery
+  def self.execute_query(username)
+    new.query(username)
+  end
+
+  def query(username)
+    validate_response(username)
+  end
+
+  private
+
+  def validate_response(username)
+    response = make_query(username)
+    return build_failure_response(username) unless response["user"]
+    return response["user"]
+  end
+
+  def make_query(username)
+    github_service(GQLi::DSL.query {
+      user(login: username) {
+        repositories(last: 100) {
+          nodes {
+            languages(last: 10) {
+              edges {
+                size
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    })
+  end
+
+  def github_service(query_data)
+    GithubApiInterface.get(query_data)
+  end
+
+  def build_failure_response(username)
+    {error: "User #{username} Not Found"}
+  end
+end
