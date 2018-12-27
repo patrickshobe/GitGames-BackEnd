7e1a3218fa2a2dc34404db7ceab0d4bb14425ef2
Tara Craig <tara.craig7@gmail.com>
2018-12-27 16:54:23 -0700

    Add method to narrow response from language query

--- app/parsers/language_parser.rb
+++ app/parsers/language_parser.rb
@@ -6,6 +6,22 @@ class LanguageParser
   end
 
   def get_data(username)
+    response = LanguageQuery.execute_query(username)
+    if response[:error]
+      @languages_breakdown = response
+    else
+      narrow_response(response)
+    end
+  end
+
+  private
+
+  def narrow_response(response)
+    narrowed_response = response["repositories"]["nodes"]
+    isolate_nodes(narrowed_response)
+  end
+
+  def isolate_nodes(narrowed_response)
 
   end
 
