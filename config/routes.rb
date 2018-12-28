394f72a712ea30d34cb5e6e95ca0d20385355d1b
Tara Craig <tara.craig7@gmail.com>
2018-12-28 12:48:32 -0700

    Add route for languages index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index]
       resources :commit_messages, only: [:index]
+      resources :languages, only: [:index]
     end
   end
 end
