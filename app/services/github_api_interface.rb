1c7a9baab58a6417e1ec5ba349ae1e903991282a
Patrick Shobe <patrick@patrickshobe.com>
2019-01-01 10:32:13 -0700

    Add ::force_get to github service The GQLi gem was failing to validate the the commit timeline query for an unknown reason, the query is valid for github. Added::force_get and #force_execute to execute the query with no query validation. This allows for the other queries to continue to be validated.

--- app/services/github_api_interface.rb
+++ app/services/github_api_interface.rb
@@ -12,6 +12,11 @@ class GithubApiInterface
     strip_result(result)
   end
 
+  def force_execute_query(query)
+    result = @github.execute!(query)
+    strip_result(result)
+  end
+
   def strip_result(result)
     result.data
   end
@@ -19,5 +24,10 @@ class GithubApiInterface
   def self.get(query)
     github = new
     github.execute_query(query)
+  end
+
+  def self.force_get(query)
+    github = new
+    github.force_execute_query(query)
   end
 end
