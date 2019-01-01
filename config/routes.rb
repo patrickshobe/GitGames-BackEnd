2f17e68c36cb11fa8739deb5cc0950f5c8f49ee3
Patrick Shobe <patrick@patrickshobe.com>
2019-01-01 13:31:46 -0700

    Add commit timeline controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index]
       resources :commit_messages, only: [:index]
+      resources :commit_timelines, only: [:index]
       resources :languages, only: [:index]
+      resources :users, only: [:index]
     end
   end
 end
