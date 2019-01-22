637e1f435bd2735a712a3abc5940a7e93a25696e
Tara Craig <tara.craig7@gmail.com>
2019-01-21 19:41:15 -0700

    Update query to reflect methods moved to query helper

--- app/queries/repository_count_query.rb
+++ app/queries/repository_count_query.rb
@@ -1,19 +1,11 @@
 class RepositoryCountQuery
+  include QueryHelper
+
   def self.execute_query(username)
     new.query(username)
   end
 
-  def query(username)
-    validate_response(username)
-  end
-
   private
-
-  def validate_response(username)
-    response = query_maker(username)
-    return build_failure_response(username) unless response["user"]
-    return response["user"]
-  end
 
   def query_maker(username)
     github_service(GQLi::DSL.query {
@@ -26,13 +18,5 @@ class RepositoryCountQuery
         }
       }
     })
-  end
-
-  def github_service(query_data)
-    GithubApiInterface.get(query_data)
-  end
-
-  def build_failure_response(username)
-    {error: "User #{username} Not Found"}
   end
 end
